f1ded762e6b4da6b4b987945ea26a110f932d8ba
sjkadali <skadalikat@gmail.com>
2020-12-10 17:02:50 -0500

    Products, App, index... changes

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons';
+import classes from '*.module.css';
+
+const Product = ({product}) => {
+    return (
+        <Card  className={classes.root}>
+            <CardMedia className={classes.media} image='' title={product.name}/>
+            <CardContent>
+                <div className={classes.CardContent}>
+                    <Typography variant="h5" gutterBottom>
+                        {product.name}
+                    </Typography>
+                    <Typography variant="h5">
+                        {product.price}
+                    </Typography>
+                </div>
+                <Typography variant="h2" color="textSecondary">{product.description}</Typography>
+            </CardContent>
+            <cardActions disableSpacing className={classes.cardActions}>
+                <IconButton aria-label="Add to Cart">
+                    <AddShoppingCart />
+                </IconButton>
+            </cardActions>
+        </Card>
+        )
+}
+
+export default Product
