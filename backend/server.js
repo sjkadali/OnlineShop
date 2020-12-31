36b9604c92ec63d35fbc443be2f252b0fb231e8c
sjkadali <skadalikat@gmail.com>
2020-12-30 21:57:18 -0500

    Mongodb setup

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,27 @@
 import express from 'express';
 import data from './data';
+import dotenv from 'dotenv';
+import config from './config';
+import mongoose from 'mongoose';
+import userRoute from './routes/userRoute';
+
+dotenv.config();
+const mongodbUrl = config.MONGODB_URL;
+mongoose.connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).catch(error => console.log(console.log(error.reason)));
 
 const app = express();
-
+app.use(function (req, res, next) {
+    res.setHeader(
+      'Content-Security-Policy-Report-Only',
+      "default-src 'self'; font-src 'self'; img-src 'self' https://images.unsplash.com; script-src 'self'; style-src 'self' https://fonts.googleapis.com https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css; frame-src 'self'"    );
+    next();
+  });
+  
+app.use("/api/users", userRoute);
 app.get("/api/products", (req, res) => {
     res.send(data.products);
 });
