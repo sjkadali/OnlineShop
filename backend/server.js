418f2b8270aacacd190fe75ec3181be6a2edcd7d
sjkadali <skadalikat@gmail.com>
2021-02-02 22:18:51 -0500

    publish to heroku

--- backend/server.js
+++ backend/server.js
@@ -40,6 +40,9 @@ app.get('/api/config/paypal', (req, res) => {
 
 const _dirname = path.resolve();
 app.use('/uploads', express.static(path.join(_dirname, '/uploads')));
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req,res) => 
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html')));
 app.use((err, res, next) => {
   res.status(500).send({message: err.message});
 });
