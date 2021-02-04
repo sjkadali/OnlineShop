b902675d5aeb6b7524e119b602771db4dc0db883
sjkadali <skadalikat@gmail.com>
2021-02-03 23:15:28 -0500

    heroku

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ app.get('/api/config/paypal', (req, res) => {
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-if(process.env.NODE_ENV === 'production') {
+if(process.env.NODE_ENV === 'production' ) {
   app.use(express.static(path.join(__dirname, '/frontend/build')));
   app.get('*', (req,res) => 
     res.sendFile(path.join(__dirname, '/frontend/build/index.html')));
